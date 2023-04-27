1c47cb1dd06bf29f9e7315fddb6005911518c44e
KevinLAnthony <kevinanthony79@yahoo.com>
2023-04-27 00:06:06 -0400

    Stylistic Update - Cinema.py
    Added spaces between some of the lines for clarity and tried to keep text within 79 character line limit.

--- Cinema.py
+++ Cinema.py
@@ -364,20 +364,28 @@ class Cinema:
     def print_results(self):
         # Print the Cinema
         self.print_cinema()
+
         # Print the groups
         print('Total Groups: ' + str(len(self.groups)))
+
         # Print the number of groups that were seated
         print('Seated Groups: ' + str(self.num_seated_groups))
+
         # Print the percentage of groups that were seated
         print('Seated Groups (%): ' + str(self.percent_seated))
+
         # Print the number of total possible seats
         print('Total Seats: ' + str(self.num_seats))
+
         # Print the number of seats that were occupied
         print('Occupied Seats: ' + str(self.num_occupied))
+
         # Print the number of people who wanted to attend
         print('Tickets Purchased: ' + str(self.num_people))
+
         # Print the percentage of seats that were occupied
         print('Occupied Seats (%): ' + str(self.percent_occupied))
+        
         # Print the percentage of people who wanted to attend that were seated
         print('Seated People (%): ' + str(self.percent_people_seated))
 
