77fff8c76a0dc233dc373d02ca0c33b02b1d5f2a
KevinLAnthony <kevinanthony79@yahoo.com>
2023-04-27 23:24:45 -0400

    Update Cinema.py
    Moved code to next lines so 79 character line limits weren't exceeded.

--- Cinema.py
+++ Cinema.py
@@ -356,14 +356,17 @@ class Cinema:
                     self.num_occupied += 1
 
         # Calculate the percentage of groups that were seated
-        self.percent_seated = round(((self.num_seated_groups / len(self.groups)) * 100), 2)
+        self.percent_seated = round(\
+            ((self.num_seated_groups / len(self.groups)) * 100), 2)
 
         # Calculate the percentage of seats that were occupied
-        self.percent_occupied = round(((self.num_occupied / self.num_seats) * 100), 2)
+        self.percent_occupied = round(\
+            ((self.num_occupied / self.num_seats) * 100), 2)
 
         # Calculate the percentage of people who wanted to attend that were 
         #   seated
-        self.percent_people_seated = round(((self.num_occupied / self.num_people) * 100), 2)
+        self.percent_people_seated = round(\
+            ((self.num_occupied / self.num_people) * 100), 2)
     
     # Prints the results of seating the groups in the Cinema
     # This has to be called after the groups have been seated
